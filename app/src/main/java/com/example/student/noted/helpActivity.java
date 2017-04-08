8e9b17d320f49558078161191e7301546bc60ce4
Beth <beth.stelzer@yahoo.com>
2017-04-08 13:09:56 -0700

    Beth Caught Up

--- app/src/main/java/com/example/student/noted/helpActivity.java
+++ app/src/main/java/com/example/student/noted/helpActivity.java
@@ -1,7 +1,9 @@
 package com.example.student.noted;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class helpActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class helpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+    }
+    public void sendNew(View view)
+    {
+        Intent intent = new Intent(helpActivity.this, newActivity.class);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/student/noted/savedActivity.java
+++ app/src/main/java/com/example/student/noted/savedActivity.java
@@ -1,7 +1,9 @@
 package com.example.student.noted;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class savedActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class savedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved);
+    }
+    public void sendNew(View view)
+    {
+        Intent intent = new Intent(savedActivity.this, newActivity.class);
+        startActivity(intent);
+
     }
 }
